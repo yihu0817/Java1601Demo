7a61590cedefb61e767ca576ce866eaf3ef222d1
viktor.zhou <2915858910@qq.com>
2016-04-13 11:49:19 +0800

    内部类

---
+++ src/com/scxh/java1601/object/innerclass/Child.java
@@ -0,0 +1,10 @@
+package com.scxh.java1601.object.innerclass;
+
+public class Child extends Person {
+
+	@Override
+	public void eat() {
+		System.out.println("eat something");
+	}
+
+}
---
+++ src/com/scxh/java1601/object/innerclass/Person.java
@@ -0,0 +1,5 @@
+package com.scxh.java1601.object.innerclass;
+
+public abstract class Person {
+	public abstract void eat();
+}
