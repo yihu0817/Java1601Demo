ebe717f8d27bb70d928c6e2696da8a9e50327495
viktor.zhou <2915858910@qq.com>
2016-04-21 16:31:19 +0800

    反射

--- src/com/scxh/java1601/reflect/ex01/ReflectDemo.java
+++ src/com/scxh/java1601/reflect/ex01/ReflectDemo.java
@@ -1,5 +1,6 @@
 package com.scxh.java1601.reflect.ex01;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -7,18 +8,22 @@ import com.scxh.java1601.util.Log;
 
 public class ReflectDemo {
 
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		A obja = new A();
-		obja.method1();
+	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
+//		A obja = new A();
+//		obja.method1();
 		
 		Class claze = Class.forName("com.scxh.java1601.reflect.ex01.A");
+		Constructor constructor = claze.getConstructor(null); //调用类必须要有默认构造方法
+		A obj = (A) constructor.newInstance(null);
 		Method m = claze.getDeclaredMethod("method2", String.class);
-		m.invoke(obja, new String("反射调用"));
+		m.invoke(obj, new String("反射调用"));
 	}
 
 }
 
 class A{
+	public A(){
+	}
 	public void method1(){
 		Log.d("调用类A 的111a方法");
 	}
